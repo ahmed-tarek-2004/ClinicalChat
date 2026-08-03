eeeaecbe41c01a22915503e30012bcd30a366c5a
Ahmed Tarek Zaher <122125653+ahmed-tarek-2004@users.noreply.github.com>
2026-08-03 20:48:38 +0300

    Update config.js

--- files/js/config.js
+++ files/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // تأكد من وضع بورت الـ API الخاص بك هنا
-  API_BASE_URL: "https://localhost:7165", 
+  API_BASE_URL: "https://clinicalparmacy.runasp.net", 
   
   ENDPOINTS: {
     LOGIN: "/api/Account/Login",
